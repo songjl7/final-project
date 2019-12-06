bf8741162c2af000c898acfce90ad85233e33ed4
LiAn Song <songlian7@gmail.com>
2019-12-05 19:11:22 -0700

    added script and variables

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function ready() {
-    let thyme = document.forms.time; 
-    let time1 = thyme.elements.firsttime; 
-    let time2 = thyme.elements.secondtime;
-    
+    let thyme = document.forms.times; 
+    let timeo = thyme.elements.firsttime;
+    let timet = thyme.elements.secondtime;
+    var time1 = new Date(timeo);
+    var time2 = new Date(timet);
+    var Time_Difference = time2.getTime() - time1.getTime(); 
     submit.onclick = function () { // go is a button on the page
-        alert("Please don't go!");
+        let el = document.createElement('p')
+        el.innerHTML = "Your elapsed time was: " + Time_Difference;
+        console.log("worked")
+        results.append(el);
     }
 }
 
