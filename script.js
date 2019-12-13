ab05e76d96cfe149a4b34e8b32b1766ea55ce53c
LiAn Song <songlian7@gmail.com>
2019-12-13 08:37:16 -0700

    tried to fix javascript

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function ready() {
-    let thyme = document.forms.times; 
-    let timeo = thyme.elements.firsttime;
-    let timet = thyme.elements.secondtime;
+    let timeo = document.getElementById("timeo").value;
+    let timet = document.getElementById("timet").value;
     var time1 = new Date(timeo);
     var time2 = new Date(timet);
-    var Day_Difference = time2.getDay() - time1.getDay(); 
-    var Hour_Difference = time2.getHours() - time1.getHours(); 
-    var Minute_Difference = time2.getMinutes() - time1.getMinutes(); 
-
+    var diff = Math.abs(time2 - time1);
+    console.log(time1);
+    console.log(time2);
     submit.onclick = function () { // go is a button on the page
         let el = document.createElement('p')
-        el.innerHTML = "Your elapsed time was: " + Day_Difference + " " + Hour_Difference + " " + Minute_Difference;
-        console.log("worked")
+        el.innerHTML = "Your elapsed time was: " + diff;
+        console.log("worked");
         results.append(el);
     }
 }
