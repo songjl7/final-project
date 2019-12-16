bd20a7f93fd8e47b4b5bcafd1ca1c064a88be6a0
LiAn Song <songlian7@gmail.com>
2019-12-16 13:29:25 -0700

    fixed time to display in days, hours, minutes, seconds, and milliseconds

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 function ready() {
-    var oneDay = document.getElementById("red");
-    var oneHour = document.getElementById("blue");
+    var oneDay = document.createElement("img");
+    oneDay.src = "red.jpg";    
+    var oneHour = document.createElement("img");
     var fifteenMin = document.getElementById("green");
     var fiveMin = document.getElementById("orange");
     var oneMin = document.getElementById("purple");
@@ -13,8 +14,24 @@ function ready() {
         console.log(timet);
         var diff = Math.abs(new Date(timet) - new Date(timeo));
 
+        function msToTime(duration) {
+            var milliseconds = parseInt((duration % 1000) / 100),
+              seconds = Math.floor((duration / 1000) % 60),
+              minutes = Math.floor((duration / (1000 * 60)) % 60),
+              hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+              days = Math.floor((duration / (1000 * 60 * 60 * 24)))
+            days = (days < 10) ? "0" + days : days;
+            hours = (hours < 10) ? "0" + hours : hours;
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
+            seconds = (seconds < 10) ? "0" + seconds : seconds;
+          
+            return days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds, and " + milliseconds + " milliseconds.";
+        }
         let el = document.createElement('p')
-        el.innerHTML = "Your elapsed time was: " + diff + "milliseconds";
+        let stuff = document.createElement('img')
+
+        if (diff )
+        el.innerHTML = "Your elapsed time: " + msToTime(diff);
         console.log("worked");
         results.append(el);
     }
