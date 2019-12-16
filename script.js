bc85dfc831c4f1d1bc4bac2768f04cd3ad3cb4f7
LiAn Song <songlian7@gmail.com>
2019-12-15 21:26:54 -0700

    revamped visuals guide, assigned images to js variables

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 function ready() {
+    var oneDay = document.getElementById("red");
+    var oneHour = document.getElementById("blue");
+    var fifteenMin = document.getElementById("green");
+    var fiveMin = document.getElementById("orange");
+    var oneMin = document.getElementById("purple");
+    
     submit.onclick = function () { // go is a button on the page
         var timeo = document.getElementById("first").value;
         var timet = document.getElementById("second").value;
         console.log(timeo);
         console.log(timet);
         var diff = Math.abs(new Date(timet) - new Date(timeo));
-        
+
         let el = document.createElement('p')
-        el.innerHTML = "Your elapsed time was: " + diff;
+        el.innerHTML = "Your elapsed time was: " + diff + "milliseconds";
         console.log("worked");
         results.append(el);
     }
